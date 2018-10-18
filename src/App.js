c51138c5edbdd8cdd688e9e6f173dd0ec2bd36c8
Aliaksandr_Minchankou <Aliaksandr_Minchankou@epam.com>
2018-10-18 10:28:08 +0300

    Fetch all books and render them

--- src/App.js
+++ src/App.js
@@ -3,9 +3,38 @@ import {hot} from "react-hot-loader";
 import "./App.css";
 
 class App extends React.Component {
+    //todo: render menu item by state (use corresponding template)
+    constructor() {
+        super();
+        this.state = {
+            'books': []
+        }
+    }
+
+    componentDidMount() {
+        this.getBooks();
+    }
+
+    getBooks() {
+        fetch('http://localhost:3000/books')
+            .then(results => results.json())
+            .then(results => this.setState({books: results}))
+    }
+
     render(){
         return(
             <div className="App">
+                {this.state.books.map((book, index) => {
+                    return (
+                        <div className='book'>
+                            <div>{book.id}</div>
+                            <div>{book.title}</div>
+                            <div>{book.author}</div>
+                            <div>{book.page_count}</div>
+                            <div>{book.year}</div>
+                        </div>
+                    )
+                })}
                 <h1> Hello, World!! </h1>
             </div>
         );
